5fa478000b1e652b9de7e65c5efef835f328368e
Brendan McGuire <brendan@bren.app>
2023-08-16 19:22:15 -0500

    Fix array sort in place issue, and make results semantically a table

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,13 @@ import {
 } from "@heroicons/react/24/solid";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import {
-  TeamRecord,
   useEvent,
   useEventExcellenceAwards,
   useEventRankings,
   useEventSkills,
   useEventTeams,
 } from "./util/event";
-import { Grade, Team } from "robotevents/out/endpoints/teams";
-import { Ranking } from "robotevents/out/endpoints/rankings";
-import { Skill } from "robotevents/out/endpoints/skills";
-import { Award } from "robotevents/out/endpoints/award";
+import { Team } from "robotevents/out/endpoints/teams";
 import { Event } from "robotevents/out/endpoints/events";
 
 function App() {
@@ -72,8 +68,62 @@ function App() {
     <>
       <header>
         <h1 className="text-3xl mb-4 text-center">Excellence Eligibility</h1>
-
-        <section className="flex md:items-center gap-4 md:flex-row flex-col">
+        <section className="mt-8">
+          <p>
+            The{" "}
+            <a
+              href="https://roboticseducation.org/documents/2023/06/guide-to-judging.pdf/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Guide to Judging
+            </a>{" "}
+            for the 2023-2024 season states that the following conditions must
+            be met for a team to be eligible for the Excellence Award:
+          </p>
+          <ol className="list-decimal list-inside m-4">
+            <li>
+              Be at or near the top of all Engineering Notebook Rubric rankings.
+            </li>
+            <li>Exhibit a high-quality team interview.</li>
+            <li>
+              Be ranked in the top 30% of teams at the conclusion of qualifying
+              matches.
+            </li>
+            <li>
+              Be ranked in the top 30% of teams at the conclusion of the Robot
+              Skills Challenges
+            </li>
+            <li>
+              Be ranked in the top 30% of Autonomous Coding Challenge rankings
+              at the conclusion of the Robot Skills Challenges
+            </li>
+            <li>Be a candidate in consideration for other Judged Awards</li>
+            <li>Demonstrate a student-centered ethos.</li>
+            <li>
+              Exhibit positive team conduct, good sportsmanship, and
+              professionalism
+            </li>
+          </ol>
+          <p>
+            The purpose of this utility is to help Judges quickly determine
+            which teams are currently eligible for the Excellence Award at their
+            event.
+          </p>
+          <p className="mt-4">
+            Disclaimer: This utility is designed to assist Judges at an event.
+            Competitors, please remember that judges consider many other factors
+            when making decisions besides what can be automatically checked. The
+            purpose of this tool is to assist judges in making decisions, not to
+            allow you to give judges a hard time if you don't win.{" "}
+            <em>
+              As always, be mindful of &lt;G1&gt; and the REC Foundation Code of
+              Conduct when competing in VIQRC, VRC, and VEXU events.
+            </em>
+          </p>
+          <p className="mt-4">To begin, enter your event's SKU below.</p>
+        </section>
+        <section className="flex md:items-center gap-4 md:flex-row flex-col mt-8">
           <input
             type="text"
             pattern="RE-(VRC|VIQRC|VEXU)-[0-9]{2}-[0-9]{4}"
@@ -130,84 +180,22 @@ function App() {
             />
           ))}
       </main>
-      <section className="mt-8">
-        <p>
-          The{" "}
-          <a
-            href="https://roboticseducation.org/documents/2023/06/guide-to-judging.pdf/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Guide to Judging
-          </a>{" "}
-          for the 2023-2024 season states that the following conditions must be
-          met for a team to be eligible for the Excellence Award:
-        </p>
-        <ol className="list-decimal list-inside m-4">
-          <li>
-            Be at or near the top of all Engineering Notebook Rubric rankings.
-          </li>
-          <li>Exhibit a high-quality team interview.</li>
-          <li>
-            Be ranked in the top 30% of teams at the conclusion of qualifying
-            matches.
-          </li>
-          <li>
-            Be ranked in the top 30% of teams at the conclusion of the Robot
-            Skills Challenges
-          </li>
-          <li>
-            Be ranked in the top 30% of Autonomous Coding Challenge rankings at
-            the conclusion of the Robot Skills Challenges
-          </li>
-          <li>Be a candidate in consideration for other Judged Awards</li>
-          <li>Demonstrate a student-centered ethos.</li>
-          <li>
-            Exhibit positive team conduct, good sportsmanship, and
-            professionalism
-          </li>
-        </ol>
-        <p>
-          The purpose of this utility is to help Judges quickly determine which
-          teams are currently eligible for the Excellence Award at their event.
-        </p>
-        <p className="mt-4">
-          Disclaimer: This utility is designed to assist Judges at an event.
-          Competitors, please remember that judges consider many other factors
-          when making decisions besides what can be automatically checked. The
-          purpose of this tool is to assist judges in making decisions, not to
-          allow you to give judges a hard time if you don't win.{" "}
-          <em>
-            As always, be mindful of &lt;G1&gt; and the REC Foundation Code of
-            Conduct when competing in VIQRC, VRC, and VEXU events.
-          </em>
-        </p>
-      </section>
     </>
   );
 }
 
 type AwardEvaluationProps = {
   event: Event | null | undefined;
-  teams: {
-    overall: Team[];
-    grades: Partial<Record<Grade, Team[]>>;
-  };
-  rankings: {
-    overall: Ranking[];
-    grades: Partial<Record<Grade, Ranking[]>>;
-  };
-  excellence: {
-    award: Award;
-    grade: Grade | "Overall";
-  };
-  skills: {
-    teamSkills: Record<string, TeamRecord>;
-    overall: TeamRecord[];
-    grades: {
-      [k: string]: TeamRecord[];
-    };
-  };
+  teams: Exclude<ReturnType<typeof useEventTeams>["data"], undefined>;
+  rankings: Exclude<
+    ReturnType<typeof useEventRankings>["data"],
+    undefined | null
+  >;
+  excellence: Exclude<
+    ReturnType<typeof useEventExcellenceAwards>["data"],
+    null | undefined
+  >[0];
+  skills: Exclude<ReturnType<typeof useEventSkills>["data"], undefined>;
 };
 
 const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
@@ -227,7 +215,7 @@ const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
       ? props.skills.overall
       : props.skills.grades[props.excellence.grade];
 
-  const autoRankings = skills.sort(
+  const autoRankings = [...skills].sort(
     (a, b) => (b.programming?.score ?? 0) - (a.programming?.score ?? 0)
   );
 
@@ -237,49 +225,61 @@ const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
   function isTeamEligible(team: Team) {
     // Top 30% of teams at the conclusion of qualifying matches
     let rankingCriterion = { eligible: false, rank: 0, reason: "" };
-    const rank =
+    const qualifyingRank =
       rankings!.findIndex((ranking) => ranking.team.id === team.id) + 1;
 
-    if (!rank) {
-      rankingCriterion = { eligible: false, rank, reason: "No Data" };
-    } else if (rank > threshold) {
-      rankingCriterion = { eligible: false, rank, reason: "Rank Too Low" };
+    if (!qualifyingRank) {
+      rankingCriterion = {
+        eligible: false,
+        rank: qualifyingRank,
+        reason: "No Data",
+      };
+    } else if (qualifyingRank > threshold) {
+      rankingCriterion = {
+        eligible: false,
+        rank: qualifyingRank,
+        reason: "Rank Too Low",
+      };
     } else {
-      rankingCriterion = { eligible: true, rank, reason: `Rank ${rank}` };
+      rankingCriterion = {
+        eligible: true,
+        rank: qualifyingRank,
+        reason: `Rank ${qualifyingRank}`,
+      };
     }
 
     // Top 30% of autonomous coding skills
     let autoSkillsCriterion = { eligible: false, reason: "" };
-    const autonomousCodingSkills =
+    const autoSkillsRank =
       autoRankings.findIndex(
         (ranking) => ranking.programming?.team.id === team.id
       ) + 1;
 
-    if (!autonomousCodingSkills) {
+    if (!autoSkillsRank) {
       autoSkillsCriterion = { eligible: false, reason: "No Data" };
-    } else if (autonomousCodingSkills > threshold) {
+    } else if (autoSkillsRank > threshold) {
       autoSkillsCriterion = {
         eligible: false,
-        reason: `Auto Skills Rank Too Low ${autonomousCodingSkills}`,
+        reason: `Auto Skills Rank Too Low ${autoSkillsRank}`,
       };
     } else {
       autoSkillsCriterion = {
         eligible: true,
-        reason: `Auto Skills Rank ${autonomousCodingSkills}`,
+        reason: `Auto Skills Rank ${autoSkillsRank}`,
       };
     }
 
     // Top 30% of overall skills
     let skillsCriterion = { eligible: false, reason: "" };
-    const overallSkills =
+    const overallSkillsRank =
       skills.findIndex((record) => {
         const number = record.driver?.team.id ?? record.programming?.team.id;
         return number === team.id;
       }) + 1;
 
-    if (!overallSkills) {
+    if (!overallSkillsRank) {
       skillsCriterion = { eligible: false, reason: "No Data" };
-    } else if (overallSkills > threshold) {
+    } else if (overallSkillsRank > threshold) {
       skillsCriterion = {
         eligible: false,
         reason: "Overall Skills Rank Too Low",
@@ -287,7 +287,7 @@ const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
     } else {
       skillsCriterion = {
         eligible: true,
-        reason: `Overall Skills Rank ${overallSkills}`,
+        reason: `Overall Skills Rank ${overallSkillsRank}`,
       };
     }
 
@@ -310,64 +310,70 @@ const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
       <p>
         Top 30% Threshold: {(teamsAtEvent * 0.3).toFixed(2)} ⟶ {threshold}
       </p>
-      <div className="mt-4 grid md:grid-cols-4 gap-1">
-        <p className="hidden md:block">Team</p>
-        <p className="hidden md:block">Ranking</p>
-        <p className="hidden md:block">Overall Skills</p>
-        <p className="hidden md:block">Autonomous Coding Skills</p>
-        {teams!.map((team) => {
-          const { eligible, ranking, autoSkills, skills } =
-            isTeamEligible(team);
-          return (
-            <>
-              <div className="mt-4 md:mt-0">
-                {eligible ? (
-                  <CheckCircleIcon
-                    height={18}
-                    className="inline text-green-400"
-                  />
-                ) : (
-                  <XCircleIcon height={18} className="inline text-red-400" />
-                )}
-                <span
-                  className={`ml-2 font-mono px-2 rounded-md ${
-                    eligible ? "bg-green-400 text-black" : "bg-transparent"
-                  }`}
-                >
-                  {team.number}
-                </span>
-              </div>
-              <div className="text-gray-400 flex justify-between">
-                <span
-                  className={`${
-                    ranking.eligible ? "text-green-400" : "text-red-400"
-                  }`}
-                >
-                  {ranking.reason}
-                </span>
-              </div>
-              <div className="text-gray-400 flex justify-between">
-                <span
-                  className={`${
-                    skills.eligible ? "text-green-400" : "text-red-400"
-                  }`}
-                >
-                  {skills.reason}
-                </span>
-              </div>
-              <div className="text-gray-400 flex justify-between">
-                <span
-                  className={`${
-                    autoSkills.eligible ? "text-green-400" : "text-red-400"
-                  }`}
-                >
-                  {autoSkills.reason}
-                </span>
-              </div>
-            </>
-          );
-        })}
-      </div>
+      <table className="w-full mt-4">
+        <thead className="text-left sr-only md:not-sr-only">
+          <tr>
+            <th>Team</th>
+            <th>Ranking</th>
+            <th>Overall Skills</th>
+            <th>Autonomous Coding Skills</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams!.map((team) => {
+            const { eligible, ranking, autoSkills, skills } =
+              isTeamEligible(team);
+            return (
+              <tr key={team.id}>
+                <td className="mt-4 md:mt-0 block md:table-cell">
+                  {eligible ? (
+                    <CheckCircleIcon
+                      height={18}
+                      className="inline text-green-400"
+                    />
+                  ) : (
+                    <XCircleIcon height={18} className="inline text-red-400" />
+                  )}
+                  <span
+                    className={`ml-2 font-mono px-2 rounded-md ${
+                      eligible ? "bg-green-400 text-black" : "bg-transparent"
+                    }`}
+                  >
+                    {team.number}
+                  </span>
+                </td>
+                <td className="text-gray-400 block md:table-cell">
+                  <span
+                    className={`${
+                      ranking.eligible ? "text-green-400" : "text-red-400"
+                    }`}
+                  >
+                    {ranking.reason}
+                  </span>
+                </td>
+                <td className="text-gray-400 block md:table-cell">
+                  <span
+                    className={`${
+                      skills.eligible ? "text-green-400" : "text-red-400"
+                    }`}
+                  >
+                    {skills.reason}
+                  </span>
+                </td>
+                <td className="text-gray-400 block md:table-cell">
+                  <span
+                    className={`${
+                      autoSkills.eligible ? "text-green-400" : "text-red-400"
+                    }`}
+                  >
+                    {autoSkills.reason}
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 };
