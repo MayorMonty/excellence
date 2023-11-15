d96463aa858e747d352f0c34fd06cf193fdb6ec3
Brendan McGuire <brendan@bren.app>
2023-11-14 23:44:43 -0600

    Refactor: Split out Award Evaluation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import { useCallback, useMemo, useState } from "react";
+import { useState } from "react";
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
-  XCircleIcon,
 } from "@heroicons/react/24/solid";
-import { ArrowDownTrayIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import {
   useEvent,
   useEventExcellenceAwards,
@@ -13,10 +12,8 @@ import {
   useEventSkills,
   useEventTeams,
   useEventsToday,
-} from "./util/event";
-import { Event } from "robotevents/out/endpoints/events";
-import { getTeamEligibility } from "./util/eligibility";
-import * as csv from "csv-stringify/browser/esm/sync";
+} from "./util/eventHooks";
+import AwardEvaluation from "./components/awardevaluation";
 
 function App() {
   const [sku, setSku] = useState("");
@@ -59,6 +56,7 @@ function App() {
     isLoadingTeams ||
     isLoadingRankings ||
     isLoadingSkills;
+
   const isFetched =
     isFetchedEvent &&
     isFetchedAwards &&
@@ -235,198 +233,5 @@ function App() {
     </>
   );
 }
-
-type AwardEvaluationProps = {
-  event: Event | null | undefined;
-  teams: Exclude<ReturnType<typeof useEventTeams>["data"], undefined>;
-  rankings: Exclude<
-    ReturnType<typeof useEventRankings>["data"],
-    undefined | null
-  >;
-  excellence: Exclude<
-    ReturnType<typeof useEventExcellenceAwards>["data"],
-    null | undefined
-  >[0];
-  skills: Exclude<ReturnType<typeof useEventSkills>["data"], undefined>;
-};
-
-export interface TeamEligibilityCriterion {
-  eligible: boolean;
-  reason: string;
-  rank: number;
-}
-
-export interface TeamEligibility {
-  eligible: boolean;
-  ranking: TeamEligibilityCriterion;
-  autoSkills: TeamEligibilityCriterion & { score: number };
-  skills: TeamEligibilityCriterion & { score: number };
-}
-
-const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
-  const teams =
-    props.excellence.grade === "Overall"
-      ? props.teams.overall
-      : props.teams.grades[props.excellence.grade] ?? [];
-  const award = props.excellence.award;
-
-  const rankings =
-    props.excellence.grade === "Overall"
-      ? props.rankings.overall
-      : props.rankings.grades[props.excellence.grade] ?? [];
-
-  const skills =
-    props.excellence.grade === "Overall"
-      ? props.skills.overall ?? []
-      : props.skills.grades[props.excellence.grade] ?? [];
-
-  const teamsAtEvent = rankings.length ?? 0;
-  const threshold = Math.round(teamsAtEvent * 0.3);
-
-  const teamEligibility = useMemo(() => {
-    if (!teams) return [];
-    return getTeamEligibility({ teams, rankings, skills, threshold });
-  }, [teams]);
-
-  const eligibleTeams = useMemo(() => {
-    if (!teams) return [];
-    return teams.filter((_, i) => teamEligibility[i].eligible);
-  }, [teamEligibility, teams]);
-
-  const onExportToCSV = useCallback(() => {
-    const data = csv.stringify(teamEligibility, {
-      header: true,
-      columns: [
-        { key: "team.number", header: "Team" },
-        { key: "eligible", header: "Eligible" },
-        { key: "ranking.eligible", header: "Ranking Eligible" },
-        { key: "ranking.reason", header: "Ranking Reason" },
-        { key: "ranking.rank", header: "Ranking Rank" },
-        { key: "skills.eligible", header: "Overall Skills Eligible" },
-        { key: "skills.reason", header: "Overall Skills Reason" },
-        { key: "skills.rank", header: "Overall Skills Rank" },
-        { key: "skills.score", header: "Overall Skills Score" },
-        { key: "autoSkills.eligible", header: "Auto Skills Eligible" },
-        { key: "autoSkills.reason", header: "Auto Skills Reason" },
-        { key: "autoSkills.rank", header: "Auto Skills Rank" },
-        { key: "autoSkills.score", header: "Auto Skills Score" },
-      ],
-      cast: {
-        boolean: (value) => (value ? "TRUE" : "FALSE"),
-      },
-    });
-    const blob = new Blob([data], { type: "text/csv" });
-    const url = URL.createObjectURL(blob);
-
-    const filename = [
-      props.event?.sku,
-      props.excellence.grade.toLowerCase().replace(/ /g, "_"),
-      "excellence.csv",
-    ].join("_");
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.setAttribute("download", filename);
-    a.click();
-  }, [teamEligibility]);
-
-  return (
-    <section className="mt-4">
-      <h2 className="font-bold">{award.title}</h2>
-      <p>Teams At Event: {teamsAtEvent}</p>
-      <p>
-        Top 30% Threshold: {(teamsAtEvent * 0.3).toFixed(2)} ⟶ {threshold}
-      </p>
-      <p className="mt-4">
-        Teams Eligible For Excellence:{" "}
-        <span className="italic">
-          {eligibleTeams.length === 0 ? "None" : null}
-        </span>
-      </p>
-      <ul className="flex flex-wrap gap-2 mt-2">
-        {eligibleTeams.map((team) => (
-          <li className="bg-green-400 text-black px-2 font-mono rounded-md">
-            {team.number}
-          </li>
-        ))}
-      </ul>
-      <nav className="flex items-center mt-2 justify-end">
-        <button
-          className="font-mono flex gap-2 items-center bg-purple-600 px-2 py-1 rounded-md hover:bg-purple-400 active:bg-purple-400"
-          title="Export as CSV"
-          onClick={onExportToCSV}
-        >
-          <ArrowDownTrayIcon height={18} />
-          csv
-        </button>
-      </nav>
-      <table className="w-full mt-4">
-        <thead className="text-left sr-only md:not-sr-only">
-          <tr>
-            <th>Team</th>
-            <th>Qualifications</th>
-            <th>Overall Skills Rank</th>
-            <th>Autonomous Coding Skills</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.map((team, i) => {
-            const { eligible, ranking, autoSkills, skills } =
-              teamEligibility[i];
-            return (
-              <tr key={team.id}>
-                <td className="mt-4 md:mt-0 block md:table-cell">
-                  {eligible ? (
-                    <CheckCircleIcon
-                      height={18}
-                      className="inline text-green-400"
-                    />
-                  ) : (
-                    <XCircleIcon height={18} className="inline text-red-400" />
-                  )}
-                  <span
-                    className={`ml-2 font-mono px-2 rounded-md ${
-                      eligible ? "bg-green-400 text-black" : "bg-transparent"
-                    }`}
-                  >
-                    {team.number}
-                  </span>
-                </td>
-
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      ranking.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {ranking.reason}
-                  </span>
-                </td>
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      skills.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {skills.reason}
-                  </span>
-                </td>
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      autoSkills.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {autoSkills.reason}
-                  </span>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </section>
-  );
-};
 
 export default App;
--- src/util/eventHooks.ts
+++ src/util/eventHooks.ts
@@ -1,7 +1,9 @@
 import * as robotevents from "robotevents";
-import { useQuery } from "react-query";
+import { UseQueryResult, useQuery } from "react-query";
 import { Grade } from "robotevents/out/endpoints/teams";
 import { Skill } from "robotevents/out/endpoints/skills";
+import { Award } from "robotevents/out/endpoints/award";
+import { Ranking } from "robotevents/out/endpoints/rankings";
 
 const ROBOTEVENTS_TOKEN =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiYjM5Y2I1NGNhMTk0OTM0ODNmNTc0MDQ2MTRhZDY0MDZjYTY1ZmQzMjAzNDlhMmM5YmUwOThlNmJjNzhhZWJmZmZjYzU0ZWY2MTQ2ZmQyYjEiLCJpYXQiOjE2ODc2NDIzODcuNTUwMjg4LCJuYmYiOjE2ODc2NDIzODcuNTUwMjkxMSwiZXhwIjoyNjM0NDE3MTg3LjUzNzIzNjIsInN1YiI6Ijk3MDY5Iiwic2NvcGVzIjpbXX0.k0DEt3QRKkgZnyV8X9mDf6VYyc8aOsIEfQbVN4Gi6Csr7O5ILLGFENXZouvplqbcMDdQ8gBMMLg5hIR38RmrTsKcWHMndq1T8wYkGZQfRhc_uZYLQhGQCaanf_F_-gnKocFwT1AKQJmAPkAbV-Itb2UzHeGpNuW8vV_TaNL3coaYvmM6rubwBuNYgyZhTHW_Mgvzh5-XBqqGpmQLm9TGl4gkeqnS-6a5PfoqRTc8v3CQWSCURFry5BA2oXz0lcWmq92FY5crr2KKv1O3chPr--oMba97elY0y9Dw0q2ipKcTm4pE7bbFP8t7-a_RKU4OyXuHRIQXjw3gEDCYXY5Hp22KMY0idnRIPhat6fybxcRfeyzUzdnubRBkDMNklwlgNCyeu2ROqEOYegtu5727Wwvy2I-xW-ZVoXg0rggVu7jVq6zmBqDFIcu50IS9R4P6a244pg2STlBaAGpzT2VfUqCBZrbtBOvdmdNzxSKIkl1AXeOIZOixo1186PX54p92ehXfCbcTgWrQSLuAAg_tBa6T7UFKFOGecVFo3v0vkmE__Q5-701f1qqcdDRNlOG-bzzFh9QLEdJWlpEajwYQ1ZjTAlbnBpKy3IrU0Aa-Jr0aqxtzgr5ZlghNtOcdYYRw5_BN0BOMmAnkvtm0_xzIJSsFbWJQJ8QpPk_n4zKZf-Y";
@@ -18,9 +20,14 @@ export function useEvent(sku: string) {
     });
 }
 
+export type EventExcellenceAwards = {
+    grade: "Overall" | Grade;
+    award: Award;
+};
+
 export function useEventExcellenceAwards(
     event: robotevents.events.Event | null | undefined
-) {
+): UseQueryResult<EventExcellenceAwards[] | null> {
     return useQuery(["excellence_awards", event?.sku], async () => {
         if (!event) {
             return null;
@@ -59,9 +66,14 @@ export function useEventExcellenceAwards(
     });
 }
 
+export type EventTeams = {
+    overall: robotevents.teams.Team[];
+    grades: Partial<Record<Grade, robotevents.teams.Team[]>>;
+}
+
 export function useEventTeams(
     event: robotevents.events.Event | null | undefined
-) {
+): UseQueryResult<EventTeams> {
     return useQuery(["teams", event?.sku], async () => {
         if (!event) {
             return { overall: [], grades: {} };
@@ -74,9 +86,15 @@ export function useEventTeams(
     });
 }
 
+export type EventRankings = {
+    overall: Ranking[];
+    grades: Partial<Record<Grade, Ranking[]>>;
+
+};
+
 export function useEventRankings(
     event: robotevents.events.Event | null | undefined
-) {
+): UseQueryResult<EventRankings> {
     const { data: teams } = useEventTeams(event);
 
     return useQuery(["rankings", event?.sku, teams], async () => {
@@ -108,9 +126,15 @@ export type TeamRecord = {
     overall: number;
 };
 
+export type EventSkills = {
+    teamSkills: Record<string, TeamRecord>;
+    overall: TeamRecord[];
+    grades: Partial<Record<Grade, TeamRecord[]>>;
+}
+
 export function useEventSkills(
     event: robotevents.events.Event | null | undefined
-) {
+): UseQueryResult<EventSkills> {
     const { data: teams } = useEventTeams(event);
 
     return useQuery(["skills", event?.sku, teams], async () => {
@@ -161,7 +185,7 @@ export function useEventSkills(
     });
 }
 
-export function useEventsToday() {
+export function useEventsToday(): UseQueryResult<robotevents.events.Event[]> {
 
     const currentSeasons = (["VRC", "VEXU", "VIQRC"] as const).map(program => robotevents.seasons.current(program)) as number[];
 
@@ -179,7 +203,6 @@ export function useEventsToday() {
             end: tomorrow.toISOString(),
             season: currentSeasons,
         });
-
 
         return events.sort((a, b) => a.name.localeCompare(b.name));
     });
