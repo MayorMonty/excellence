8b46c30b45a9566f6a5a98bf4e1997d0eac2faea
Brendan McGuire <brendan@bren.app>
2024-06-20 22:37:56 -0500

    cleanup: move team eligibility to it's own component

--- src/components/AwardEvaluation.tsx
+++ src/components/AwardEvaluation.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useMemo } from "react";
-import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
-import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import {
   EventExcellenceAwards,
   EventRankings,
@@ -9,10 +8,11 @@ import {
   EventTeamsByDivision,
 } from "../util/eventHooks";
 import { Event } from "robotevents/out/endpoints/events";
-import { getTeamEligibility } from "../util/eligibility";
+import { getTeamEligibility, TeamEligibility } from "../util/eligibility";
 import * as csv from "csv-stringify/browser/esm/sync";
+import { TeamEligibilityTable } from "./TeamEligibility";
 
-type AwardEvaluationProps = {
+export type AwardEvaluationProps = {
   event: Event | null | undefined;
   rankings: EventRankings;
   divisionTeams: EventTeamsByDivision;
@@ -21,43 +21,6 @@ type AwardEvaluationProps = {
   excellence: EventExcellenceAwards;
   skills: EventSkills;
 };
-
-export interface TeamEligibilityCriterion {
-  eligible: boolean;
-  reason: string;
-  rank: number;
-}
-
-export interface TeamEligibility {
-  eligible: boolean;
-  ranking: TeamEligibilityCriterion;
-  autoSkills: TeamEligibilityCriterion & { score: number };
-  skills: TeamEligibilityCriterion & { score: number };
-}
-
-function toCSVString(teamEligibility: TeamEligibility[]) {
-  return csv.stringify(teamEligibility, {
-    header: true,
-    columns: [
-      { key: "team.number", header: "Team" },
-      { key: "eligible", header: "Eligible" },
-      { key: "ranking.eligible", header: "Ranking Eligible" },
-      { key: "ranking.reason", header: "Ranking Reason" },
-      { key: "ranking.rank", header: "Ranking Rank" },
-      { key: "skills.eligible", header: "Overall Skills Eligible" },
-      { key: "skills.reason", header: "Overall Skills Reason" },
-      { key: "skills.rank", header: "Overall Skills Rank" },
-      { key: "skills.score", header: "Overall Skills Score" },
-      { key: "autoSkills.eligible", header: "Auto Skills Eligible" },
-      { key: "autoSkills.reason", header: "Auto Skills Reason" },
-      { key: "autoSkills.rank", header: "Auto Skills Rank" },
-      { key: "autoSkills.score", header: "Auto Skills Score" },
-    ],
-    cast: {
-      boolean: (value) => (value ? "TRUE" : "FALSE"),
-    },
-  });
-}
 
 const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
   const division = props.event?.divisions.find(
@@ -187,73 +150,33 @@ const AwardEvaluation: React.FC<AwardEvaluationProps> = (props) => {
           csv
         </button>
       </nav>
-      <table className="w-full mt-4">
-        <thead className="text-left sr-only md:not-sr-only">
-          <tr>
-            <th>Team</th>
-            <th>Qualification Rank</th>
-            <th>Overall Skills Rank</th>
-            <th>Autonomous Coding Skills</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.map((team, i) => {
-            const { eligible, ranking, autoSkills, skills } =
-              teamEligibility[i];
-            return (
-              <tr key={team.id}>
-                <td className="mt-4 md:mt-0 block md:table-cell">
-                  {eligible ? (
-                    <CheckCircleIcon
-                      height={18}
-                      className="inline text-green-400"
-                    />
-                  ) : (
-                    <XCircleIcon height={18} className="inline text-red-400" />
-                  )}
-                  <span
-                    className={`ml-2 font-mono px-2 rounded-md ${
-                      eligible ? "bg-green-400 text-black" : "bg-transparent"
-                    }`}
-                  >
-                    {team.number}
-                  </span>
-                </td>
-
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      ranking.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {ranking.reason}
-                  </span>
-                </td>
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      skills.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {skills.reason}
-                  </span>
-                </td>
-                <td className="text-gray-400 block md:table-cell">
-                  <span
-                    className={`${
-                      autoSkills.eligible ? "text-green-400" : "text-red-400"
-                    }`}
-                  >
-                    {autoSkills.reason}
-                  </span>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TeamEligibilityTable teams={teamEligibility} />
     </section>
   );
 };
 
 export default AwardEvaluation;
+
+function toCSVString(teamEligibility: TeamEligibility[]) {
+  return csv.stringify(teamEligibility, {
+    header: true,
+    columns: [
+      { key: "team.number", header: "Team" },
+      { key: "eligible", header: "Eligible" },
+      { key: "ranking.eligible", header: "Ranking Eligible" },
+      { key: "ranking.reason", header: "Ranking Reason" },
+      { key: "ranking.rank", header: "Ranking Rank" },
+      { key: "skills.eligible", header: "Overall Skills Eligible" },
+      { key: "skills.reason", header: "Overall Skills Reason" },
+      { key: "skills.rank", header: "Overall Skills Rank" },
+      { key: "skills.score", header: "Overall Skills Score" },
+      { key: "autoSkills.eligible", header: "Auto Skills Eligible" },
+      { key: "autoSkills.reason", header: "Auto Skills Reason" },
+      { key: "autoSkills.rank", header: "Auto Skills Rank" },
+      { key: "autoSkills.score", header: "Auto Skills Score" },
+    ],
+    cast: {
+      boolean: (value) => (value ? "TRUE" : "FALSE"),
+    },
+  });
+}
